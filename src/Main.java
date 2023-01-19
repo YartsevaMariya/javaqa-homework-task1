6874ed9ee13d259c315d1bf53a71035ee3ff1776
Yartseva Maria <kammariya@mail.ru>
2023-01-19 15:10:31 +0300

    Solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = 1211;
+        int bonus = 20;
+
+        int mile = price / bonus;
+        System.out.println(" Начисленные мили " + mile);
     }
+
 }
